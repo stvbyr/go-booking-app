39775e0cf29611173ea16672b87925b3c68c8605
Steve <beyer_steve@outlook.de>
2022-02-06 10:32:02 +0100

    first program

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to our conference booking application.")
+	fmt.Println("Get your tickets here.")
+
+	conferenceName := "Go Conference"
+	fmt.Println(conferenceName)
+}
